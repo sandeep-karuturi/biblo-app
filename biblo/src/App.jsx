c0574c8af6c354248b551251083252556154c340
Sandeep Karuturi <sandeepkk1938@gmail.com>
2025-12-28 00:23:41 +0530

    Add static pages: About, Help, Privacy, Terms

--- biblo/src/App.jsx
+++ biblo/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import MySwaps from './pages/MySwaps';
 import Register from './pages/Register';
